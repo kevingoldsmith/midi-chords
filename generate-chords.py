cd56ed390a697bf6c1a221b8d7a801d792bedade
Kevin Goldsmith <kevin@unitcircle.com>
2020-12-03 23:35:28 -0800

    added maj7, min7, dom7, sus2, sus4, aug, dom9, maj11 now goes from C0 -> G9, drops any chords that go out of range

--- generate-chords.py
+++ generate-chords.py
@@ -2,16 +2,48 @@ from midiutil import MIDIFile
 
 chords = [
     {
-        'name': 'major',
+        'name': 'maj',
         'offsets': [0,4,7]
     },
     {
-        'name': 'minor',
+        'name': 'min',
         'offsets': [0, 3, 7]
     },
     {
         'name': 'dim',
         'offsets': [0, 3, 9]
+    },
+    {
+        'name': 'maj7',
+        'offsets': [0, 4, 7, 11]
+    },
+    {
+        'name': 'min7',
+        'offsets': [0, 3, 7, 10]
+    },
+    {
+        'name': 'dom7',
+        'offsets': [0, 4, 7, 10]
+    },
+    {
+        'name': 'sus2',
+        'offsets': [0, 2, 7]
+    },
+    {
+        'name': 'sus4',
+        'offsets': [0, 5, 7]
+    },
+    {
+        'name': 'aug',
+        'offsets': [0, 4, 8]
+    },
+    {
+        'name': 'dom9',
+        'offsets': [0, 4, 7, 10, 14]
+    },
+    {
+        'name': 'maj11',
+        'offsets': [0, 4, 7, 11, 14, 17]
     }
 ]
 
@@ -22,22 +54,23 @@ notes = [
 start_note = 24
 
 midi_note = start_note
-for octave in range(0,5):
+for octave in range(0,9):
     for note in notes:
         for chord in chords:
-            print(f'{note} {octave} {chord["name"]} {midi_note} {midi_note + chord["offsets"][1]} {midi_note + chord["offsets"][2]}')
-            track    = 0
-            channel  = 0
-            time     = 0    # In beats
-            duration = 4    # In beats
-            tempo    = 120  # In BPM
-            volume   = 100  # 0-127, as per the MIDI standard
+            if midi_note + chord['offsets'][-1] < 128:
+                print(f'{note} {octave} {chord["name"]}')
+                track    = 0
+                channel  = 0
+                time     = 0    # In beats
+                duration = 4    # In beats
+                tempo    = 120  # In BPM
+                volume   = 100  # 0-127, as per the MIDI standard
 
-            MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created automatically)
-            MyMIDI.addTempo(track, time, tempo)
-            for offset in chord['offsets']:
-                MyMIDI.addNote(track, channel, midi_note + offset, 0, duration, volume)
+                MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created automatically)
+                MyMIDI.addTempo(track, time, tempo)
+                for offset in chord['offsets']:
+                    MyMIDI.addNote(track, channel, midi_note + offset, 0, duration, volume)
 
-            with open(f"{note} {octave} {chord['name']}.mid", "wb") as output_file:
-                MyMIDI.writeFile(output_file)
+                with open(f"{note} {octave} {chord['name']}.mid", "wb") as output_file:
+                    MyMIDI.writeFile(output_file)
         midi_note += 1
