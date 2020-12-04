054727d9fba51eb0b314013f20d4464e9ef63519
Kevin Goldsmith <kevin@unitcircle.com>
2020-12-03 22:48:26 -0800

    generate major chords

---
+++ generate-chords.py
@@ -0,0 +1,35 @@
+from midiutil import MIDIFile
+
+chords = [
+    {
+        'name': 'major',
+        'offsets': [0,4,7]
+    }
+]
+
+notes = [
+    'C', 'C#-Db', 'D', 'D#-Eb', 'E', 'F', 'F#-Gb', 'G', 'G#-Ab', 'A', 'A#-Bb', 'B'
+]
+
+start_note = 24
+
+midi_note = start_note
+for octave in range(0,5):
+    for note in notes:
+        for chord in chords:
+            print(f'{note} {octave} {chord["name"]} {midi_note} {midi_note + chord["offsets"][1]} {midi_note + chord["offsets"][2]}')
+            track    = 0
+            channel  = 0
+            time     = 0    # In beats
+            duration = 4    # In beats
+            tempo    = 120  # In BPM
+            volume   = 100  # 0-127, as per the MIDI standard
+
+            MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created automatically)
+            MyMIDI.addTempo(track, time, tempo)
+            for offset in chord['offsets']:
+                MyMIDI.addNote(track, channel, midi_note + offset, 0, duration, volume)
+
+            with open(f"{note} {octave} major-chord.mid", "wb") as output_file:
+                MyMIDI.writeFile(output_file)
+            midi_note += 1
